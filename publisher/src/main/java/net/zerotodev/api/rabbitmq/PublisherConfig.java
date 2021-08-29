d67707df91c92807dcd13ca00d28e4d51dd8c57f
parkjungkwan <pakjkwan@gmail.com>
2021-08-29 15:49:03 +0900

    first commit

---
+++ publisher/src/main/java/net/zerotodev/api/rabbitmq/PublisherConfig.java
@@ -0,0 +1,38 @@
+package net.zerotodev.api.rabbitmq;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.converter.MessageConverter;
+
+public class PublisherConfig {
+    private static final String EXCHANGE_NAME = "sample.exchange";
+    private static final String QUEUE_NAME = "sample.queue";
+    private static final String ROUTING_KEY = "sample.key";
+
+    @Bean
+    TopicExchange exchange(){
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+    @Bean
+    Queue queue(){
+        return new Queue(QUEUE_NAME);
+    }
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange){
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+    @Bean
+    RabbitTemplate rabbitTemplate(ConnectionFactory factory, MessageConverter converter){
+        RabbitTemplate template = new RabbitTemplate(factory);
+        template.setMessageConverter(new Jackson2JsonMessageConverter());
+        return template;
+    }
+
+
+}
